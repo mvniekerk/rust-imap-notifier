9e3b91da4f3bb0c69576d40b13b7ebb8a18ed832
Michael van Niekerk <mvanniekerk@jhb.dvt.co.za>
2019-05-05 10:20:10 +0200

    Initial scaffolding

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+
+extern crate yaml_rust;
+use yaml_rust::{YamlLoader, YamlEmitter};
+
+extern crate imap;
+extern crate native_tls;
+
+fn fetch_inbox_top() -> imap::error::Result<Option<String>> {
+    let domain = "imap.example.com";
+    let tls = native_tls::TlsConnector::builder().build().unwrap();
+
+    // we pass in the domain twice to check that the server's TLS
+    // certificate is valid for the domain we're connecting to.
+    let client = imap::connect((domain, 993), domain, &tls).unwrap();
+
+    // the client we have here is unauthenticated.
+    // to do anything useful with the e-mails, we need to log in
+    let mut imap_session = client
+        .login("me@example.com", "password")
+        .map_err(|e| e.0)?;
+
+    // we want to fetch the first email in the INBOX mailbox
+    imap_session.select("INBOX")?;
+
+    // fetch message number 1 in this mailbox, along with its RFC822 field.
+    // RFC 822 dictates the format of the body of e-mails
+    let messages = imap_session.fetch("1", "RFC822")?;
+    let message = if let Some(m) = messages.iter().next() {
+        m
+    } else {
+        return Ok(None);
+    };
+
+    // extract the message's body
+    let body = message.body().expect("message did not have a body!");
+    let body = std::str::from_utf8(body)
+        .expect("message was not valid utf-8")
+        .to_string();
+
+    // be nice to the server and log out
+    imap_session.logout()?;
+
+    Ok(Some(body))
+}
+
+fn yaml() {
+        let s =
+"
+foo:
+    - list1
+    - list2
+bar:
+    - 1
+    - 2.0
+";
+    let docs = YamlLoader::load_from_str(s).unwrap();
+
+    // Multi document support, doc is a yaml::Yaml
+    let doc = &docs[0];
+
+    // Debug support
+    println!("{:?}", doc);
+
+    // Index access for map & array
+    assert_eq!(doc["foo"][0].as_str().unwrap(), "list1");
+    assert_eq!(doc["bar"][1].as_f64().unwrap(), 2.0);
+
+    // Chained key/array access is checked and won't panic,
+    // return BadValue if they are not exist.
+    assert!(doc["INVALID_KEY"][100].is_badvalue());
+
+    // Dump the YAML object
+    let mut out_str = String::new();
+    {
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(doc).unwrap(); // dump the YAML object to a String
+    }
+    println!("{}", out_str);
+}
+
+fn main() {
+    println!("Hello, world!");
+    notifica::notify("<message title>", "<message body>");
+    yaml();
+}
